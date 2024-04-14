a6032893a4c0150f575f06b3541a5062654f69d4
OgaDavid <oluwademiladedavidoga@gmail.com>
2024-04-14 14:53:43 +0100

    add description to generateActions.ts

--- src/helpers/generateActions.ts
+++ src/helpers/generateActions.ts
@@ -1,3 +1,10 @@
+/* 
+This function generates action types based on the provided action name.
+
+@param action - The action name as a string.
+@return An object containing three action types: REQUEST, SUCCESS, and FAILURE.
+*/
+
 export const generateActions = (action: string) => {
   action = action.toUpperCase();
   return {
